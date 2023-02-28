f658aea734e6bde1c90680a530e39dcfd644a574
Nataleesha <frojlan@gmail.com>
2023-02-28 04:52:59 +0200

    3

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,4 @@
-import {
-  renderResponse,
-  renderRawResponse,
-  renderJsonResponse,
-} from "./renderFunctions";
+import { renderResponse } from "./renderFunctions";
 
 const API_KEY = "dc42d62a4cda439f87bf91bacf04925c";
 const URL = "https://api.rebrandly.com/v1/links";
@@ -11,32 +7,25 @@ const inputField = document.querySelector("#input");
 const shortenButton = document.querySelector("#shorten");
 const responseField = document.querySelector("#responseField");
 
-const shortenUrl = () => {
+const shortenUrl = async () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({ destination: urlToShorten });
-
-  fetch(URL, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-      apikey: API_KEY,
-    },
-    body: data,
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Request failed!");
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+        apikey: API_KEY,
       },
-      (networkError) => {
-        console.log(networkError.message);
-      }
-    )
-    .then((jsonResponse) => {
-      renderResponse(jsonResponse);
     });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const displayShortUrl = (event) => {
